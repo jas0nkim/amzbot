276f5c90045a753c5a0c25412d6bad41f3df4985
Jason Kim <jason.kim.jiho@gmail.com>
2020-04-15 00:13:54 -0400

    removed unused lines

--- amzbot/settings.py
+++ amzbot/settings.py
@@ -105,20 +105,3 @@ AMAZON_COM_ITEM_VARIATION_LINK_POSTFIX = '/?th=1&psc=1'
 AMAZON_COM_ITEM_IMAGE_CONVERT_PATTERN_FROM = r'\._([^_]+)_\.'
 AMAZON_COM_ITEM_IMAGE_CONVERT_STRING_TO_PRIMARY = '._SL1500_.'
 AMAZON_COM_ITEM_IMAGE_CONVERT_STRING_TO_SECONDARY = '._SX522_.'
-
-
-
-from pathlib import Path
-to_add=Path('/Users/jason/Projects/amzbot/amzbot/djg_resources')
-from sys import path
-
-if str(to_add) not in path:
-    minLen=999999
-    for index,directory in enumerate(path):
-        if 'site-packages' in directory and len(directory)<=minLen:
-            minLen=len(directory)
-            stpi=index
-
-    pathSitePckgs=Path(path[stpi])
-    with open(pathSitePckgs/'current_machine_paths.pth','w') as pth_file:
-        pth_file.write(str(to_add))
