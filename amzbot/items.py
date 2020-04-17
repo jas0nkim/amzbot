3dab34f78408eec00a48015bd00597fee1f0dca8
Jason Kim <jason.kim.jiho@gmail.com>
2020-04-16 21:06:23 -0400

    improved DjangoItem (AmzbotDjangoItem) - override DjangoItem.save in order to handle 'update'

--- amzbot/items.py
+++ amzbot/items.py
@@ -10,13 +10,40 @@ utils.setup_djg()
 
 from scrapy import Field
 from scrapy_djangoitem import DjangoItem
+from django.forms.models import model_to_dict
 from djg_resources.models import AmazonParentListing, AmazonListing
 
 
-class ParentListingItem(DjangoItem):
+class AmzbotDjangoItem(DjangoItem):
+    def save(self, commit=True):
+        """ override DjangoItem.save in order to handle 'update'
+        """
+        if commit:
+            model_class = type(self.instance)
+            pk = self.instance.pk
+            if pk is None:
+                raise Exception("Primary Key not found. Unable to save to database.")
+            update_instance = True
+            destination = None
+            try:
+                destination = model_class.objects.get(pk=pk)
+            except model_class.DoesNotExist:
+                update_instance = False
+            if update_instance:
+                source_dict = model_to_dict(self.instance)
+                for (key, value) in source_dict.items():
+                    if value is not None:
+                        setattr(destination, key, value)
+                # update self._instance
+                self._instance = destination
+            self.instance.save()
+        return self.instance
+
+
+class ParentListingItem(AmzbotDjangoItem):
     django_model = AmazonParentListing
 
-class ListingItem(DjangoItem):
+class ListingItem(AmzbotDjangoItem):
     django_model = AmazonListing
     _redirected_asins = Field()
     _cached = Field()
