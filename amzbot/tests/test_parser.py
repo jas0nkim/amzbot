6235c459931854362256cfaabeb34ee0c3bb588b
Jason Kim <jason.kim.jiho@gmail.com>
2020-04-19 17:44:26 -0400

    separated tests - now each elements has own tests

--- amzbot/tests/test_parser.py
+++ amzbot/tests/test_parser.py
@@ -24,33 +24,118 @@ def build_response(url, html_filename, domain):
 class TestParser(unittest.TestCase):
     def setUp(self):
         self.testlist = utils.get_testlist(self.__class__.__name__)
-    
-    def __test_item(self, item, t):
-        if item.__class__.__name__ == 'ParentListingItem':
-            self.__test_parent_listing_item(item, t)
-        elif item.__class__.__name__ == 'ListingItem':
-            self.__test_listing_item(item, t)
-        else:
-            raise Exception("Invalid 'item' passed")
-
-    def __test_parent_listing_item(self, item, t):
-        self.assertEqual(item['review_count'], t['expected_review_count'])
-        self.assertEqual(item['avg_rating'], t['expected_avg_rating'])
-
-    def __test_listing_item(self, item, t):
-        self.assertEqual(item['asin'], t['expected_asin'])
-        self.assertEqual(item['title'], t['expected_title'])
-        self.assertEqual(item['price'], t['expected_price'])
-        self.assertEqual(item['is_fba'], t['expected_is_fba'])
-        self.assertEqual(item['is_addon'], t['expected_is_addon'])
-        self.assertEqual(item['is_pantry'], t['expected_is_pantry'])
-        self.assertEqual(item['brand_name'], t['expected_brand_name'])
-        self.assertEqual(item['status'], t['expected_status'])
-
-    def test_parse_amazon_item(self):
         for t in self.testlist:
-            for item in parse_amazon_item(build_response(t['url'], t['html_filename'], t['domain'])):
-                self.__test_item(item, t)
+            t['items'] = list(parse_amazon_item(build_response(t['url'], t['html_filename'], t['domain'])))
+    
+    def test_asin(self):
+        for t in self.testlist:
+            for i in t['items']:
+                with self.subTest(asin=t['expected_asin']):
+                    if i.__class__.__name__ == 'ListingItem':
+                        self.assertEqual(i['asin'], t['expected_asin'])
+                    elif i.__class__.__name__ == 'ParentListingItem':
+                        pass
+                    else:
+                        raise Exception("Invalid 'item' object passed - {}".format(i.__class__.__name__))
+
+    def test_title(self):
+        for t in self.testlist:
+            for i in t['items']:
+                with self.subTest(asin=t['expected_asin']):
+                    if i.__class__.__name__ == 'ListingItem':
+                        self.assertEqual(i['title'], t['expected_title'])
+                    elif i.__class__.__name__ == 'ParentListingItem':
+                        pass
+                    else:
+                        raise Exception("Invalid 'item' object passed - {}".format(i.__class__.__name__))
+
+    def test_price(self):
+        for t in self.testlist:
+            for i in t['items']:
+                with self.subTest(asin=t['expected_asin']):
+                    if i.__class__.__name__ == 'ListingItem':
+                        self.assertEqual(i['price'], t['expected_price'])
+                    elif i.__class__.__name__ == 'ParentListingItem':
+                        pass
+                    else:
+                        raise Exception("Invalid 'item' object passed - {}".format(i.__class__.__name__))
+
+    def test_avg_rating(self):
+        for t in self.testlist:
+            for i in t['items']:
+                with self.subTest(asin=t['expected_asin']):
+                    if i.__class__.__name__ == 'ListingItem':
+                        pass
+                    elif i.__class__.__name__ == 'ParentListingItem':
+                        self.assertEqual(i['avg_rating'], t['expected_avg_rating'])
+                    else:
+                        raise Exception("Invalid 'item' object passed - {}".format(i.__class__.__name__))
+
+    def test_review_count(self):
+        for t in self.testlist:
+            for i in t['items']:
+                with self.subTest(asin=t['expected_asin']):
+                    if i.__class__.__name__ == 'ListingItem':
+                        pass
+                    elif i.__class__.__name__ == 'ParentListingItem':
+                        self.assertEqual(i['review_count'], t['expected_review_count'])
+                    else:
+                        raise Exception("Invalid 'item' object passed - {}".format(i.__class__.__name__))
+
+    def test_is_fba(self):
+        for t in self.testlist:
+            for i in t['items']:
+                with self.subTest(asin=t['expected_asin']):
+                    if i.__class__.__name__ == 'ListingItem':
+                        self.assertEqual(i['is_fba'], t['expected_is_fba'])
+                    elif i.__class__.__name__ == 'ParentListingItem':
+                        pass
+                    else:
+                        raise Exception("Invalid 'item' object passed - {}".format(i.__class__.__name__))
+
+    def test_is_addon(self):
+        for t in self.testlist:
+            for i in t['items']:
+                with self.subTest(asin=t['expected_asin']):
+                    if i.__class__.__name__ == 'ListingItem':
+                        self.assertEqual(i['is_addon'], t['expected_is_addon'])
+                    elif i.__class__.__name__ == 'ParentListingItem':
+                        pass
+                    else:
+                        raise Exception("Invalid 'item' object passed - {}".format(i.__class__.__name__))
+
+    def test_is_pantry(self):
+        for t in self.testlist:
+            for i in t['items']:
+                with self.subTest(asin=t['expected_asin']):
+                    if i.__class__.__name__ == 'ListingItem':
+                        self.assertEqual(i['is_pantry'], t['expected_is_pantry'])
+                    elif i.__class__.__name__ == 'ParentListingItem':
+                        pass
+                    else:
+                        raise Exception("Invalid 'item' object passed - {}".format(i.__class__.__name__))
+
+    def test_brand_name(self):
+        for t in self.testlist:
+            for i in t['items']:
+                with self.subTest(asin=t['expected_asin']):
+                    if i.__class__.__name__ == 'ListingItem':
+                        self.assertEqual(i['brand_name'], t['expected_brand_name'])
+                    elif i.__class__.__name__ == 'ParentListingItem':
+                        pass
+                    else:
+                        raise Exception("Invalid 'item' object passed - {}".format(i.__class__.__name__))
+
+    def test_status(self):
+        for t in self.testlist:
+            for i in t['items']:
+                with self.subTest(asin=t['expected_asin']):
+                    if i.__class__.__name__ == 'ListingItem':
+                        self.assertEqual(i['status'], t['expected_status'])
+                    elif i.__class__.__name__ == 'ParentListingItem':
+                        pass
+                    else:
+                        raise Exception("Invalid 'item' object passed - {}".format(i.__class__.__name__))
 
 
 if __name__ == '__main__':
