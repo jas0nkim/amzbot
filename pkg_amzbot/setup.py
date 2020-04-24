dcac2b7004a32ebe05a96c5d9707f02d3f9e7e3b
Jason Kim <jason.kim.jiho@gmail.com>
2020-04-24 00:42:45 -0400

    moved 'src' under pkg_amzbot/ directory

--- pkg_amzbot/setup.py
+++ pkg_amzbot/setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
+with open('../requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(
     name = 'amzbot',
     version = '0.0.1',
     package_dir = {'': 'src'},
-    packages = find_packages(where='src', exclude=["tests", "tests.*"]),
+    packages = find_packages(where='src', exclude=["amzbot.tests", "amzbot.tests.*"]),
     install_requires = required,
     scripts = ['src/djg/manage.py'],
     entry_points = {'scrapy': ['settings = amzbot.settings']},
