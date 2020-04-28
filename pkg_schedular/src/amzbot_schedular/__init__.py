979024ccbd556531d595b6744ca588615a9b323d
Jason Kim <jason.kim.jiho@gmail.com>
2020-04-28 01:29:57 -0400

    store jobs/schedules into db

--- pkg_schedular/src/amzbot_schedular/__init__.py
+++ pkg_schedular/src/amzbot_schedular/__init__.py
@@ -7,6 +7,7 @@ django.setup()
 ## django integration end
 
 ## config, custom logger
+
 import logging, graypy
 from djg.settings import APP_CONFIG_FILEPATH, APP_DIST_DIRPATH
 
@@ -29,10 +30,16 @@ graylog_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 logger.addHandler(graylog_handler)
 
+## config, custom logger end
 
+import ast
+from datetime import datetime
 from scrapyd_api import ScrapydAPI
 from scrapyd_api.exceptions import ScrapydResponseError
+from django.core.exceptions import ObjectDoesNotExist
 from djg.settings import BASE_DIR
+from djg.schedules.models import Job, Version
+
 
 
 def class_fullname(o):
@@ -67,12 +74,18 @@ class Schedular(object):
                 num_of_spiders = self._scrapyd.add_version(project, version, egg)
         except FileNotFoundError as e:
             logger.error("{}: {}".format(class_fullname(e), str(e)))
+        except ScrapydResponseError as e:
+            logger.error("{}: Response error - {}".format(class_fullname(e), str(e)))
         except Exception as e:
             logger.error("{}: Failed to add a new version - {}".format(class_fullname(e), str(e)))
         else:
             logger.info("new version '{}' for project '{}' added - {} spider(s)".format(project, version, num_of_spiders))
-            """ TODO: store result in db
-            """
+            v = Version()
+            v.project = project
+            v.version = version
+            v.status = Version.STATUS_ADDED
+            v.added_at = datetime.now()
+            v.save()
         finally:
             return num_of_spiders
 
@@ -83,12 +96,19 @@ class Schedular(object):
         jobid = None
         try:
             jobid = self._scrapyd.schedule(project, spider, settings, **kwargs)
+        except ScrapydResponseError as e:
+            logger.error("{}: Response error - {}".format(class_fullname(e), str(e)))
         except Exception as e:
             logger.error("{}: Failed to schedule a job - {}".format(class_fullname(e), str(e)))
         else:
             logger.info("new scheduled job '{}' for project '{}', spider '{}' has been set".format(jobid, project, spider))
-            """ TODO: store result in db
-            """
+            j = Job()
+            j.project = project
+            j.version = kwargs.pop('_version', None)
+            j.settings = settings
+            j.other_params = kwargs
+            j.status = Job.STATUS_PENDING
+            j.save()
         finally:
             return jobid
 
@@ -105,12 +125,84 @@ class Schedular(object):
             logger.error("{}: Failed to list jobs - {}".format(class_fullname(e), str(e)))
         else:
             logger.info("list of jobs for project '{}' - {}".format(project, str(jobs)))
-            """ TODO: store result in db
-            """
+            self._store_jobs(jobs)
         finally:
             return jobs
 
+    def _store_jobs(self, jobs):
+        """ parse jobs and store information into db
+        """
+        try:
+            d = ast.literal_eval(jobs)
+            """ convert string to dictionaty
+                https://www.geeksforgeeks.org/python-convert-string-dictionary-to-dictionary/
+            """
+        except Exception as e:
+            logger.error("{}: Failed to parse jobs - {}".format(class_fullname(e), str(e)))
+        else:
+            for x in d['running']:
+                try:
+                    j = Job.objects.get(job_id=x['id'])
+                except ObjectDoesNotExist as e:
+                    logger.error("{}: Job '{}' doesn't exist in db - {}".format(class_fullname(e), x['id'], str(e)))
+                else:
+                    if j.status != Job.STATUS_RUNNING:
+                        j.start_time = x['start_time']
+                        j.status = Job.STATUS_RUNNING
+                        j.save()
+            for x in d['finished']:
+                try:
+                    j = Job.objects.get(job_id=x['id'])
+                except ObjectDoesNotExist as e:
+                    logger.error("{}: Job '{}' doesn't exist in db - {}".format(class_fullname(e), x['id'], str(e)))
+                else:
+                    if j.status != Job.STATUS_FINISHED:
+                        j.start_time = x['start_time']
+                        j.end_time = x['end_time']
+                        j.status = Job.STATUS_FINISHED
+                        j.save()
+
+    def delversion(self, project, version):
+        """ delversion
+        """
+        if not self._scrapyd:
+            logger.error("No scrapyd object find. Unable to delete version.")
+            return False
+        deleted = False
+        try:
+            deleted = self._scrapyd.delete_version(project, version)
+        except ScrapydResponseError as e:
+            logger.error("{}: Response error - {}".format(class_fullname(e), str(e)))
+        except Exception as e:
+            logger.error("{}: Failed to delete version - {}".format(class_fullname(e), str(e)))
+        else:
+            logger.info("successfully deleted project '{}' version '{}'".format(project, version))
+            for v in Version.objects.filter(project=project, version=version):
+                v.status = Version.STATUS_DELETED
+                v.save()
+        finally:
+            return deleted
+
+    def delproject(self, project):
+        """ delproject
+        """
+        if not self._scrapyd:
+            logger.error("No scrapyd object find. Unable to delete version.")
+            return False
+        deleted = False
+        try:
+            deleted = self._scrapyd.delete_project(project)
+        except ScrapydResponseError as e:
+            logger.error("{}: Response error - {}".format(class_fullname(e), str(e)))
+        except Exception as e:
+            logger.error("{}: Failed to delete project - {}".format(class_fullname(e), str(e)))
+        else:
+            logger.info("successfully deleted project '{}'".format(project))
+            for v in Version.objects.filter(project=project):
+                v.status = Version.STATUS_DELETED
+                v.save()
+        finally:
+            return deleted
+
     def close(self):
         self._scrapyd.client.close()
-
-
