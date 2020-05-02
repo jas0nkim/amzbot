d350b43602a87241da51728ba6c3e2439fb81e42
Jason Kim <jason.kim.jiho@gmail.com>
2020-05-02 18:29:08 -0400

    view/url changed to match altered tables

--- pkg_pricewatch_web/src/pwweb/resources/urls.py
+++ pkg_pricewatch_web/src/pwweb/resources/urls.py
@@ -1,13 +1,14 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from pwweb.resources import views
 
-# Create a router and register our viewsets with it.
-router = DefaultRouter()
-router.register('amazon_parent_listing', views.AmazonParentListingViewSet)
-router.register('amazon_listing', views.AmazonListingViewSet)
 
 urlpatterns = [
-    path('amazon_listing_price/<str:asin>/<str:domain>/', views.AmazonListingPriceList.as_view()),
-    path('', include(router.urls)),
+    path('amazon_parent_listing/', views.AmazonParentListingList.as_view()),
+    path('amazon_parent_listing/<str:parent_asin>/<str:domain>/',
+        views.AmazonParentListingDetail.as_view()),
+    path('amazon_listing/', views.AmazonListingList.as_view()),
+    path('amazon_listing/<str:asin>/<str:domain>/',
+        views.AmazonListingDetail.as_view()),
+    path('amazon_listing_price/<str:asin>/<str:domain>/',
+        views.AmazonListingPriceList.as_view()),
 ]
--- pkg_pricewatch_web/src/pwweb/resources/views.py
+++ pkg_pricewatch_web/src/pwweb/resources/views.py
@@ -1,45 +1,85 @@
+from django.http import Http404
 from rest_framework import viewsets, generics
+from rest_framework.mixins import CreateModelMixin, UpdateModelMixin
 from pwweb.resources.models import AmazonParentListing, AmazonListing, AmazonListingPrice
 from pwweb.resources.serializers import AmazonParentListingSerializer, AmazonListingSerializer, AmazonListingPriceSerializer
 from pwweb.mixins import MultipleFieldLookupMixin
 
-class AmazonParentListingViewSet(viewsets.ModelViewSet):
-    """
-    This viewset automatically provides `list`, `create`, `retrieve`,
-    `update` and `destroy` actions.
 
-    Additionally we also provide an extra `highlight` action.
-    """
+class AmazonParentListingList(CreateModelMixin, UpdateModelMixin, generics.ListAPIView):
     queryset = AmazonParentListing.objects.all()
     serializer_class = AmazonParentListingSerializer
-    # permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-    #                       IsOwnerOrReadOnly]
+    # permission_classes = [IsAdminUser]
+
+    _instance = None
+
+    def get_object(self):
+        """
+        override rest_framework.generics.GenericAPIView.get_object()
+        workaround using two lookup fields ('parent_asin', 'domain') instead of 'pk' field on fetching
+        """
+        return self._instance
+
+    def post(self, request, *args, **kwargs):
+        """ handle both create and update on post method
+        """
+        try:
+            self._instance = AmazonParentListing.objects.get(project=request.data.get('parent_asin'),
+                version=request.data.get('domain'))
+        except AmazonParentListing.DoesNotExist:
+            # create
+            return self.create(request, *args, **kwargs)
+        else:
+            # update
+            return self.update(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
 
 
-class AmazonListingViewSet(viewsets.ModelViewSet):
-    """
-    This viewset automatically provides `list`, `create`, `retrieve`,
-    `update` and `destroy` actions.
+class AmazonParentListingDetail(MultipleFieldLookupMixin, generics.RetrieveAPIView):
+    queryset = AmazonParentListing.objects.all()
+    serializer_class = AmazonParentListingSerializer
+    # permission_classes = [IsAdminUser]
+    lookup_fields = ['parent_asin', 'domain']
 
-    Additionally we also provide an extra `highlight` action.
-    """
+
+class AmazonListingList(CreateModelMixin, UpdateModelMixin, generics.ListAPIView):
     queryset = AmazonListing.objects.all()
     serializer_class = AmazonListingSerializer
-    # permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-    #                       IsOwnerOrReadOnly]
+    # permission_classes = [IsAdminUser]
+
+    _instance = None
+
+    def get_object(self):
+        """
+        override rest_framework.generics.GenericAPIView.get_object()
+        workaround using two lookup fields ('asin', 'domain') instead of 'pk' field on fetching
+        """
+        return self._instance
+
+    def post(self, request, *args, **kwargs):
+        """ handle both create and update on post method
+        """
+        try:
+            self._instance = AmazonListing.objects.get(project=request.data.get('asin'),
+                version=request.data.get('domain'))
+        except AmazonListing.DoesNotExist:
+            # create
+            return self.create(request, *args, **kwargs)
+        else:
+            # update
+            return self.update(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
 
 
-# class AmazonListingPriceViewSet(viewsets.ModelViewSet):
-#     """
-#     This viewset automatically provides `list`, `create`, `retrieve`,
-#     `update` and `destroy` actions.
-
-#     Additionally we also provide an extra `highlight` action.
-#     """
-#     queryset = AmazonListingPrice.objects.all()
-#     serializer_class = AmazonListingPriceSerializer
-#     # permission_classes = [permissions.IsAuthenticatedOrReadOnly,
-#     #                       IsOwnerOrReadOnly]
+class AmazonListingDetail(MultipleFieldLookupMixin, generics.RetrieveAPIView):
+    queryset = AmazonListing.objects.all()
+    serializer_class = AmazonListingSerializer
+    # permission_classes = [IsAdminUser]
+    lookup_fields = ['asin', 'domain']
 
 
 class AmazonListingPriceList(MultipleFieldLookupMixin, generics.ListAPIView):
@@ -61,3 +101,41 @@ class AmazonListingPriceList(MultipleFieldLookupMixin, generics.ListAPIView):
     #     serializer = self.get_serializer(instance=instance)
     #     serializer.is_valid(raise_exception=True)
     #     serializer.save()
+
+# class AmazonParentListingViewSet(viewsets.ModelViewSet):
+#     """
+#     This viewset automatically provides `list`, `create`, `retrieve`,
+#     `update` and `destroy` actions.
+
+#     Additionally we also provide an extra `highlight` action.
+#     """
+#     queryset = AmazonParentListing.objects.all()
+#     serializer_class = AmazonParentListingSerializer
+#     # permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+#     #                       IsOwnerOrReadOnly]
+
+
+# class AmazonListingViewSet(viewsets.ModelViewSet):
+#     """
+#     This viewset automatically provides `list`, `create`, `retrieve`,
+#     `update` and `destroy` actions.
+
+#     Additionally we also provide an extra `highlight` action.
+#     """
+#     queryset = AmazonListing.objects.all()
+#     serializer_class = AmazonListingSerializer
+#     # permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+#     #                       IsOwnerOrReadOnly]
+
+
+# class AmazonListingPriceViewSet(viewsets.ModelViewSet):
+#     """
+#     This viewset automatically provides `list`, `create`, `retrieve`,
+#     `update` and `destroy` actions.
+
+#     Additionally we also provide an extra `highlight` action.
+#     """
+#     queryset = AmazonListingPrice.objects.all()
+#     serializer_class = AmazonListingPriceSerializer
+#     # permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+#     #                       IsOwnerOrReadOnly]
